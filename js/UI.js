b576d4871364b72b4949c1c4e73f74ab9e9753d9
Yelena Belikova <yelenabel@gmail.com>
2015-09-25 18:27:54 -0700

    Display things added to the cart and updates total cost

--- js/UI.js
+++ js/UI.js
@@ -42,11 +42,35 @@ $(document).ready(function() {
     var menuCatergory = classes.substr(0, classes.indexOf(" "));
 
       event.preventDefault();
-      var selected-items = [];
-
-      var selected-items = $("input:checkbox:checked." + menuCatergory).map(function(){
-        searchIDs.push($(this).val());
+      var selected_items = [];
+       $("input:checkbox:checked." + menuCatergory).map(function(){
+        selected_items.push($(this).val());
       });
+      if(menuCatergory !== "toppings") {
+        selected_items.forEach(function(item) {
+          if(menuCatergory === "drinks") {
+            order.addDrink(item);
+          } else if(menuCatergory === "sides") {
+            order.addSide(item);
+          } else if(menuCatergory === "extras") {
+            order.addExtra(item);
+          } else if(menuCatergory === "desserts") {
+            order.addDessert(item);
+          }
+          $("#cart-items").append("<li>" + item + "</li>");
+          $("#total-cost").text(order.totalPrice);
+        });
+      } else if(menuCatergory === "toppings") {
+          var size = $("#pizza-size").val();
+          var pizza = new Pizza(size);
 
+          selected_items.forEach(function(item) {
+            pizza.addTopping(item);
+          });
+          pizza.calculatePrice(menu);
+          order.addPizza(pizza);
+          $("#cart-items").append("<li>" + pizza.size + " with" + selected_items +  "</li>");
+          $("#total-cost").text(order.totalPrice);
+      }
   });
 });
